cd247cf4d461d52900e3b536fc40cd0ab38bd25e
Behrouz Hasanebygi <behrouz.hasanbeygi@smartly.io>
2022-07-12 14:52:01 +0300

    tranlate route paths

--- pkg/envoy/ingress_translator.go
+++ pkg/envoy/ingress_translator.go
@@ -2,7 +2,6 @@ package envoy
 
 import (
 	"fmt"
-	"regexp"
 	"sort"
 	"strings"
 	"time"
@@ -63,61 +62,21 @@ type envoyConfiguration struct {
 	Clusters     []*cluster
 }
 
-type virtuhalhostroute struct {
-	PathMatchCondition string
-}
 type virtualHost struct {
-	Host            string
+	Host          string
+	Routes        []*Localroute
+	Timeout       time.Duration
+	PerTryTimeout time.Duration
+}
+
+type Localroute struct {
 	UpstreamCluster string
-	Timeout         time.Duration
-	PerTryTimeout   time.Duration
+	Route           *envoy_route_v3.RouteMatch
 }
 
-type Route struct {
-
-	// PathMatchCondition specifies a MatchCondition to match on the request path.
-	// Must not be nil.
-	PathMatchCondition MatchCondition
-}
-type MatchCondition interface {
-	fmt.Stringer
-}
-
-// PrefixMatchType represents different types of prefix matching alternatives.
-type PrefixMatchType int
-
-const (
-	// PrefixMatchString represents a prefix match that functions like a
-	// string prefix match, i.e. prefix /foo matches /foobar
-	PrefixMatchString PrefixMatchType = iota
-	// PrefixMatchSegment represents a prefix match that only matches full path
-	// segments, i.e. prefix /foo matches /foo/bar but not /foobar
-	PrefixMatchSegment
-)
-
-var prefixMatchTypeToName = map[PrefixMatchType]string{
-	PrefixMatchString:  "string",
-	PrefixMatchSegment: "segment",
-}
-
-// PrefixMatchCondition matches the start of a URL.
-type PrefixMatchCondition struct {
-	Prefix          string
-	PrefixMatchType PrefixMatchType
-}
-
-func (ec *ExactMatchCondition) String() string {
-	return "exact: " + ec.Path
-}
-
-// ExactMatchCondition matches the entire path of a URL.
-type ExactMatchCondition struct {
-	Path string
-}
-
-// RegexMatchCondition matches the URL by regular expression.
-type RegexMatchCondition struct {
-	Regex string
+type key struct {
+	host string
+	path string
 }
 
 func (v *virtualHost) Equals(other *virtualHost) bool {
@@ -127,7 +86,6 @@ func (v *virtualHost) Equals(other *virtualHost) bool {
 
 	return v.Host == other.Host &&
 		v.Timeout == other.Timeout &&
-		v.UpstreamCluster == other.UpstreamCluster &&
 		v.PerTryTimeout == other.PerTryTimeout
 }
 
@@ -228,206 +186,106 @@ Ingress:
 	return vi
 }
 
-type envoyIngress struct {
-	vhost   *virtualHost
-	cluster *cluster
-}
-
-func newEnvoyIngress(host string) *envoyIngress {
-	clusterName := strings.Replace(host, ".", "_", -1)
-	return &envoyIngress{
-		vhost: &virtualHost{
-			Host:            host,
-			UpstreamCluster: clusterName,
-			Timeout:         (15 * time.Second),
-			PerTryTimeout:   (5 * time.Second),
-		},
-		cluster: &cluster{
-			Name:            clusterName,
-			VirtualHost:     host,
-			Hosts:           []string{},
-			Timeout:         (30 * time.Second),
-			HealthCheckPath: "",
-		},
+func newVirtualHost(host string) *virtualHost {
+	return &virtualHost{
+		Host:          host,
+		Timeout:       (15 * time.Second),
+		PerTryTimeout: (5 * time.Second),
 	}
 }
-
-func (ing *envoyIngress) addUpstream(host string) {
-	ing.cluster.Hosts = append(ing.cluster.Hosts, host)
+func newCluster(host string) *cluster {
+	clusterName := strings.Replace(host, ".", "_", -1)
+	return &cluster{
+		Name:            clusterName,
+		VirtualHost:     host,
+		Hosts:           []string{},
+		Timeout:         (30 * time.Second),
+		HealthCheckPath: "",
+	}
+}
+func (ing *cluster) addUpstream(host string) {
+	ing.Hosts = append(ing.Hosts, host)
 }
 
-func (ing *envoyIngress) addHealthCheckPath(path string) {
-	ing.cluster.HealthCheckPath = path
+func (ing *cluster) addHealthCheckPath(path string) {
+	ing.HealthCheckPath = path
 }
 
-func (ing *envoyIngress) addTimeout(timeout time.Duration) {
-	ing.cluster.Timeout = timeout
-	ing.vhost.Timeout = timeout
-	ing.vhost.PerTryTimeout = timeout
+func (ing *virtualHost) addVhostTimeout(timeout time.Duration) {
+	ing.Timeout = timeout
+	ing.PerTryTimeout = timeout
+}
+
+func (ing *virtualHost) addlocalroute(clusternmae string, route *envoy_route_v3.RouteMatch) {
+	make := Localroute{Route: route, UpstreamCluster: clusternmae}
+	ing.Routes = append(ing.Routes, &make)
+}
+
+func (ing *cluster) addclustername(clustername string) {
+	ing.Name = clustername
 }
 
 func translateIngresses(ingresses []v1.Ingress) *envoyConfiguration {
 	cfg := &envoyConfiguration{}
-	envoyIngresses := map[string]*envoyIngress{}
+
+	clusters := map[key]*cluster{}
+	virtualhosts := map[string]*virtualHost{}
 
 	for _, i := range ingresses {
 		for _, j := range i.Status.LoadBalancer.Ingress {
 			for _, rule := range i.Spec.Rules {
-				_, ok := envoyIngresses[rule.Host]
+
+				_, ok := virtualhosts[rule.Host]
 				if !ok {
-					envoyIngresses[rule.Host] = newEnvoyIngress(rule.Host)
+					virtualhosts[rule.Host] = newVirtualHost(rule.Host)
 				}
 
-				envoyIngress := envoyIngresses[rule.Host]
+				virtualHost := virtualhosts[rule.Host]
 
 				for _, httppath := range httppaths(rule) {
+
+					_, exist := clusters[key{host: rule.Host, path: httppath.Path}]
+					if !exist {
+						clusters[key{host: rule.Host, path: httppath.Path}] = newCluster(rule.Host)
+					}
+					cluster := clusters[key{host: rule.Host, path: httppath.Path}]
+
 					path := stringOrDefault(httppath.Path, "/")
 					// Default to implementation specific path matching if not set.
 					pathType := derefPathTypeOr(httppath.PathType, v1.PathTypeImplementationSpecific)
-					fmt.Println(path, pathType)
-				}
+					clustername := fmt.Sprint(strings.Replace(rule.Host, ".", "_", -1), stringTohash(rule))
+					virtualHost.addlocalroute(clustername, RouteMatch(Pathtranslate(path, pathType)))
+					cluster.addclustername(clustername)
+					fmt.Println(clustername, path)
+					if j.Hostname != "" {
+						cluster.addUpstream(j.Hostname)
+					} else {
+						cluster.addUpstream(j.IP)
+					}
 
-				if j.Hostname != "" {
-					envoyIngress.addUpstream(j.Hostname)
-				} else {
-					envoyIngress.addUpstream(j.IP)
-				}
-
-				if i.GetAnnotations()["yggdrasil.uswitch.com/healthcheck-path"] != "" {
-					envoyIngress.addHealthCheckPath(i.GetAnnotations()["yggdrasil.uswitch.com/healthcheck-path"])
+					if i.GetAnnotations()["yggdrasil.uswitch.com/healthcheck-path"] != "" {
+						cluster.addHealthCheckPath(i.GetAnnotations()["yggdrasil.uswitch.com/healthcheck-path"])
+					}
 				}
 
 				if i.GetAnnotations()["yggdrasil.uswitch.com/timeout"] != "" {
 					timeout, err := time.ParseDuration(i.GetAnnotations()["yggdrasil.uswitch.com/timeout"])
 					if err == nil {
-						envoyIngress.addTimeout(timeout)
+						virtualHost.addVhostTimeout(timeout)
 					}
 				}
 			}
 		}
 	}
 
-	for _, ingress := range envoyIngresses {
-		cfg.Clusters = append(cfg.Clusters, ingress.cluster)
-		cfg.VirtualHosts = append(cfg.VirtualHosts, ingress.vhost)
+	for _, cluster := range clusters {
+		cfg.Clusters = append(cfg.Clusters, cluster)
 	}
-
+	for _, virtualhost := range virtualhosts {
+		cfg.VirtualHosts = append(cfg.VirtualHosts, virtualhost)
+	}
 	numVhosts.Set(float64(len(cfg.VirtualHosts)))
 	numClusters.Set(float64(len(cfg.Clusters)))
 
 	return cfg
-}
-
-func httppaths(rule v1.IngressRule) []v1.HTTPIngressPath {
-	if rule.IngressRuleValue.HTTP == nil {
-		// rule.IngressRuleValue.HTTP value is optional.
-		return nil
-	}
-	return rule.IngressRuleValue.HTTP.Paths
-}
-
-func stringOrDefault(s, def string) string {
-	if s == "" {
-		return def
-	}
-	return s
-}
-
-func derefPathTypeOr(ptr *v1.PathType, def v1.PathType) v1.PathType {
-	if ptr != nil {
-		return *ptr
-	}
-	return def
-}
-
-func Pathtranslate(path string, pathtype v1.PathType) (*Route, error) {
-	r := &Route{
-		PathMatchCondition: nil,
-	}
-	switch pathtype {
-	case v1.PathTypePrefix:
-		prefixMatchType := PrefixMatchSegment
-		// An "all paths" prefix should be treated as a generic string prefix
-		// match.
-		if path == "/" {
-			prefixMatchType = PrefixMatchString
-		} else {
-			// Strip trailing slashes. Ensures /foo matches prefix /foo/
-			path = strings.TrimRight(path, "/")
-		}
-		r.PathMatchCondition = &PrefixMatchCondition{Prefix: path, PrefixMatchType: prefixMatchType}
-	case v1.PathTypeExact:
-		r.PathMatchCondition = &ExactMatchCondition{Path: path}
-	case v1.PathTypeImplementationSpecific:
-		// If a path "looks like" a regex we give a regex path match.
-		// Otherwise you get a string prefix match.
-		if strings.ContainsAny(path, "^+*[]%") {
-			// validate the regex
-			if err := ValidateRegex(path); err != nil {
-				return nil, err
-			}
-			r.PathMatchCondition = &RegexMatchCondition{Regex: path}
-		} else {
-			r.PathMatchCondition = &PrefixMatchCondition{Prefix: path, PrefixMatchType: PrefixMatchString}
-		}
-	}
-	return r, nil
-}
-
-// RouteMatch creates a *envoy_route_v3.RouteMatch for the supplied *dag.Route.
-func RouteMatch(route Route) *envoy_route_v3.RouteMatch {
-	switch c := route.PathMatchCondition.(type) {
-	case *RegexMatchCondition:
-		return &envoy_route_v3.RouteMatch{
-			PathSpecifier: &envoy_route_v3.RouteMatch_SafeRegex{
-				// Add an anchor since we at the very least have a / as a string literal prefix.
-				// Reduces regex program size so Envoy doesn't reject long prefix matches.
-				SafeRegex: SafeRegexMatch("^" + c.Regex),
-			},
-		}
-	case *PrefixMatchCondition:
-		switch c.PrefixMatchType {
-		case PrefixMatchSegment:
-			return &envoy_route_v3.RouteMatch{
-				PathSpecifier: &envoy_route_v3.RouteMatch_PathSeparatedPrefix{
-					PathSeparatedPrefix: c.Prefix,
-				},
-			}
-		case PrefixMatchString:
-			fallthrough
-		default:
-			return &envoy_route_v3.RouteMatch{
-				PathSpecifier: &envoy_route_v3.RouteMatch_Prefix{
-					Prefix: c.Prefix,
-				},
-			}
-		}
-	case *ExactMatchCondition:
-		return &envoy_route_v3.RouteMatch{
-			PathSpecifier: &envoy_route_v3.RouteMatch_Path{
-				Path: c.Path,
-			},
-		}
-	default:
-		return &envoy_route_v3.RouteMatch{}
-	}
-}
-
-// ValidateRegex returns an error if the supplied
-// RE2 regex syntax is invalid.
-func ValidateRegex(regex string) error {
-	_, err := regexp.Compile(regex)
-	return err
-}
-
-func (rc *RegexMatchCondition) String() string {
-	return "regex: " + rc.Regex
-}
-
-func (pc *PrefixMatchCondition) String() string {
-	str := "prefix: " + pc.Prefix
-	if typeStr, ok := prefixMatchTypeToName[pc.PrefixMatchType]; ok {
-		str += " type: " + typeStr
-	}
-	return str
 }
---
+++ pkg/envoy/route.go
@@ -0,0 +1,180 @@
+// this lines of codes are copy and modified from contour project with Apache License 2.0
+
+package envoy
+
+import (
+	"fmt"
+	"hash/fnv"
+	"regexp"
+	"strings"
+
+	envoy_route_v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	v1 "k8s.io/api/networking/v1"
+)
+
+type Route struct {
+
+	// PathMatchCondition specifies a MatchCondition to match on the request path.
+	// Must not be nil.
+	PathMatchCondition MatchCondition
+}
+type MatchCondition interface {
+	fmt.Stringer
+}
+
+// PrefixMatchType represents different types of prefix matching alternatives.
+type PrefixMatchType int
+
+const (
+	// PrefixMatchString represents a prefix match that functions like a
+	// string prefix match, i.e. prefix /foo matches /foobar
+	PrefixMatchString PrefixMatchType = iota
+	// PrefixMatchSegment represents a prefix match that only matches full path
+	// segments, i.e. prefix /foo matches /foo/bar but not /foobar
+	PrefixMatchSegment
+)
+
+var prefixMatchTypeToName = map[PrefixMatchType]string{
+	PrefixMatchString:  "string",
+	PrefixMatchSegment: "segment",
+}
+
+// PrefixMatchCondition matches the start of a URL.
+type PrefixMatchCondition struct {
+	Prefix          string
+	PrefixMatchType PrefixMatchType
+}
+
+func (ec *ExactMatchCondition) String() string {
+	return "exact: " + ec.Path
+}
+
+// ExactMatchCondition matches the entire path of a URL.
+type ExactMatchCondition struct {
+	Path string
+}
+
+// RegexMatchCondition matches the URL by regular expression.
+type RegexMatchCondition struct {
+	Regex string
+}
+
+func httppaths(rule v1.IngressRule) []v1.HTTPIngressPath {
+	if rule.IngressRuleValue.HTTP == nil {
+		// rule.IngressRuleValue.HTTP value is optional.
+		return nil
+	}
+	return rule.IngressRuleValue.HTTP.Paths
+}
+
+func stringOrDefault(s, def string) string {
+	if s == "" {
+		return def
+	}
+	return s
+}
+
+func derefPathTypeOr(ptr *v1.PathType, def v1.PathType) v1.PathType {
+	if ptr != nil {
+		return *ptr
+	}
+	return def
+}
+
+func Pathtranslate(path string, pathtype v1.PathType) *Route {
+	r := &Route{
+		PathMatchCondition: nil,
+	}
+	switch pathtype {
+	case v1.PathTypePrefix:
+		prefixMatchType := PrefixMatchSegment
+		// An "all paths" prefix should be treated as a generic string prefix
+		// match.
+		if path == "/" {
+			prefixMatchType = PrefixMatchString
+		} else {
+			// Strip trailing slashes. Ensures /foo matches prefix /foo/
+			path = strings.TrimRight(path, "/")
+		}
+		r.PathMatchCondition = &PrefixMatchCondition{Prefix: path, PrefixMatchType: prefixMatchType}
+	case v1.PathTypeExact:
+		r.PathMatchCondition = &ExactMatchCondition{Path: path}
+	case v1.PathTypeImplementationSpecific:
+		// If a path "looks like" a regex we give a regex path match.
+		// Otherwise you get a string prefix match.
+		if strings.ContainsAny(path, "^+*[]%") {
+			// validate the regex
+			if err := ValidateRegex(path); err != nil {
+				return nil
+			}
+			r.PathMatchCondition = &RegexMatchCondition{Regex: path}
+		} else {
+			r.PathMatchCondition = &PrefixMatchCondition{Prefix: path, PrefixMatchType: PrefixMatchString}
+		}
+	}
+	return r
+}
+
+// RouteMatch creates a *envoy_route_v3.RouteMatch for the supplied *dag.Route.
+func RouteMatch(route *Route) *envoy_route_v3.RouteMatch {
+	switch c := route.PathMatchCondition.(type) {
+	case *RegexMatchCondition:
+		return &envoy_route_v3.RouteMatch{
+			PathSpecifier: &envoy_route_v3.RouteMatch_SafeRegex{
+				// Add an anchor since we at the very least have a / as a string literal prefix.
+				// Reduces regex program size so Envoy doesn't reject long prefix matches.
+				SafeRegex: SafeRegexMatch("^" + c.Regex),
+			},
+		}
+	case *PrefixMatchCondition:
+		switch c.PrefixMatchType {
+		case PrefixMatchSegment:
+			return &envoy_route_v3.RouteMatch{
+				PathSpecifier: &envoy_route_v3.RouteMatch_PathSeparatedPrefix{
+					PathSeparatedPrefix: c.Prefix,
+				},
+			}
+		case PrefixMatchString:
+			fallthrough
+		default:
+			return &envoy_route_v3.RouteMatch{
+				PathSpecifier: &envoy_route_v3.RouteMatch_Prefix{
+					Prefix: c.Prefix,
+				},
+			}
+		}
+	case *ExactMatchCondition:
+		return &envoy_route_v3.RouteMatch{
+			PathSpecifier: &envoy_route_v3.RouteMatch_Path{
+				Path: c.Path,
+			},
+		}
+	default:
+		return &envoy_route_v3.RouteMatch{}
+	}
+}
+
+// ValidateRegex returns an error if the supplied
+// RE2 regex syntax is invalid.
+func ValidateRegex(regex string) error {
+	_, err := regexp.Compile(regex)
+	return err
+}
+
+func (rc *RegexMatchCondition) String() string {
+	return "regex: " + rc.Regex
+}
+
+func (pc *PrefixMatchCondition) String() string {
+	str := "prefix: " + pc.Prefix
+	if typeStr, ok := prefixMatchTypeToName[pc.PrefixMatchType]; ok {
+		str += " type: " + typeStr
+	}
+	return str
+}
+
+func stringTohash(s v1.IngressRule) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(fmt.Sprintf("%v", s)))
+	return h.Sum32()
+}
